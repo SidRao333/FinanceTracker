eefe4a9ea69d817c33ab5cfffae0fe81006f2775
Godlee-CJ <88628672+Godlee-CJ@users.noreply.github.com>
2025-08-05 11:43:16 +0000

    docker file

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8000);
+  // await app.listen(process.env.PORT ?? 8000);
+  await app.listen(3000, '0.0.0.0');
+  console.log(`🚀 Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
